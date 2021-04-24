5700395903db4b75e0511c96cfe6e17f0eb1ef73
seoyeong <josyeong1011@gmail.com>
2021-04-25 03:33:59 +0900

    #2 내부 저장소 save.txt에 비밀번호를 저장 가능 비밀번호 조회에서 조회 가능

--- GUI.py
+++ GUI.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import random, engine
+from tkinter import messagebox
 
 
 class APP(Tk):
@@ -23,9 +24,18 @@ class APP(Tk):
 
 class main(Frame):
     def __init__(self, master):
-        Frame.__init__(self, master, width=400, height=300)
-        Button(self, text="비밀번호 생성하기", font=('맑은 고딕', 15), command=lambda: master.switch_frame(makePW)).place(x="110", y="70")
-        Button(self, text="암호화하기", font=('맑은 고딕', 15), command=lambda: master.switch_frame(encryption)).place(x="140", y="150")
+        Frame.__init__(self, master, width=400, height=300, bg="#FFCDD9")
+        self.makePWbtn = PhotoImage(file="other/makePWbtn.png").subsample(2)
+        self.lookupBtn = PhotoImage(file="other/makePWlookup.png").subsample(2)
+        self.encryptionBtn = PhotoImage(file="other/encryptionBtn.png").subsample(2)
+        self.decryptionBtn = PhotoImage(file="other/decryptionBtn.png").subsample(2)
+        Button(self, text="", image=self.makePWbtn, command=lambda: master.switch_frame(makePW)).place(x="60", y="60")
+        Button(self, text="", image=self.lookupBtn,
+               command=lambda: master.switch_frame(lookup_PW)).place(x="220", y="60")
+        Button(self, text="", image=self.encryptionBtn,
+               command=lambda: master.switch_frame(Encryption)).place(x="60", y="145")
+        Button(self, text="", image=self.decryptionBtn,
+               command=lambda: master.switch_frame(Decryption)).place(x="220", y="145")
         self.propagate(0)
 
 
@@ -44,17 +54,57 @@ class makePW(Frame):
         Label(self, text="결과").place(x="50", y="160")
         self.result = Entry(self, width=30)
         self.result.place(x="140", y="160")
+        Button(self, text="비밀번호 저장하기", command=self.save_window).place(x="270", y="250")
         self.propagate(0)
 
     def go(self):
-        plainText = Entry.get(self.PW)
+        plain_text = Entry.get(self.PW)
         key = Entry.get(self.PWKEY)
-        tmp = engine.쌍자치환(plainText, key)
+        tmp = engine.encryption(plain_text, key)
         self.result.delete(0, "end")
         self.result.insert(0, tmp)
 
+    def save_window(self):
+        if Entry.get(self.PW).replace(" ", "") == "":
+            messagebox.showwarning("!", "비밀번호를 입력하세요!")
+        elif Entry.get(self.PWKEY).replace(" ", "") == "":
+            messagebox.showwarning("!", "암호키를 입력하세요!")
+        elif Entry.get(self.result).replace(" ", "") == "":
+            messagebox.showwarning("!", "비밀번호를 생성하세요!")
+        else:
+            self.new_window = Toplevel(self)
+            self.new_window.title("비밀번호 저장하기")
+            self.new_window.geometry("200x80+500+300")
+            Label(self.new_window, text="저장명", anchor="w").place(x="15", y="15")
+            self.save_name = Entry(self.new_window, width=15)
+            self.save_name.place(x="65", y="15")
+            Button(self.new_window, text="저장하기", command=self.save).place(x="130", y="45")
 
-class encryption(Frame):
+    def save(self):
+        if Entry.get(self.save_name).replace(" ", "") == "":
+            messagebox.showwarning("!", "저장명을 입력하세요!")
+        else:
+            f = open('Other/save.txt', 'at')
+            f.write(f'{Entry.get(self.save_name)}\t\t{Entry.get(self.result)}\t\t{Entry.get(self.PWKEY)}\n')
+            f.close()
+            self.new_window.destroy()
+
+
+class lookup_PW(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master, width=400, height=300)
+        self.back = PhotoImage(file="other/icon_back.png").subsample(25)
+        Button(self, text=" ", image=self.back, command=lambda: master.switch_frame(main)).place(x="0", y="0")
+        Label(self, text="저장명\t\t비밀번호\t\t암호키", anchor="w").place(x="30", y="30")
+        list = open('Other/save.txt', 'rt').read().split('\n')
+        tmp = Text(self, width="45", height="10")
+        for x in list:
+            tmp.insert(END, x + "\n")
+        tmp.place(x="30", y="50")
+        self.propagate(0)
+
+
+class Encryption(Frame):
     def __init__(self, master):
         Frame.__init__(self, master, width=400, height=300)
         self.back = PhotoImage(file="other/icon_back.png").subsample(25)
@@ -74,7 +124,7 @@ class encryption(Frame):
     def go(self):
         plainText = Entry.get(self.text)
         key = self.getKey()
-        tmp = engine.쌍자치환(plainText, key)
+        tmp = engine.encryption(plainText, key)
         self.result.delete(0, "end")
         self.result.insert(0, tmp)
         self.useKey.config(text=key)
@@ -84,6 +134,32 @@ class encryption(Frame):
         key = random.choice(keys)
 
         return key
+
+
+class Decryption(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master, width=400, height=300)
+        self.back = PhotoImage(file="other/icon_back.png").subsample(25)
+        Button(self, text=" ", image=self.back, command=lambda: master.switch_frame(main)).place(x="0", y="0")
+        Label(self, text="암호문 입력").place(x="50", y="60")
+        self.PW = Entry(self, width=30)
+        self.PW.place(x="140", y="60")
+        Label(self, text="암호키 입력").place(x="50", y="90")
+        self.PWKEY = Entry(self, width=30)
+        self.PWKEY.place(x="140", y="90")
+        Button(self, text="복호화 하기", command=self.go).place(x="280", y="120")
+        Label(self, text="결과").place(x="50", y="160")
+        self.result = Entry(self, width=30)
+        self.result.place(x="140", y="160")
+        Label(self, text="결과는 정확하지 않으며 띄어쓰기가 없고 Z가 Q로 나옵니다.").place(x="40", y="200")
+        self.propagate(0)
+
+    def go(self):
+        text = Entry.get(self.PW)
+        key = Entry.get(self.PWKEY)
+        tmp = engine.Decryption(text, key)
+        self.result.delete(0, "end")
+        self.result.insert(0, tmp)
 
 
 if __name__ == "__main__":
--- engine.py
+++ engine.py
@@ -3,7 +3,7 @@ def 중복문자열제거(str):
     return result
 
 
-def 쌍자치환(string, PWkey):
+def encryption(string, PWkey):
     board = ["", "", "", "", ""], \
             ["", "", "", "", ""], \
             ["", "", "", "", ""], \
@@ -45,55 +45,141 @@ def 쌍자치환(string, PWkey):
         if count == len(alphabet):
             break
 
-    playFair = []
-    encPlayFair = []
+    play_fair = []
+    enc_play_fair = []
     x1 = 0
     x2 = 0
     y1 = 0
     y2 = 0
-    encStr = ""
+    result = ""
 
     i = 0
     while i < len(string):
-        tmpArr = [[], []]
-        tmpArr[0] = string[i]
+        tmp_arr = [[], []]
+        tmp_arr[0] = string[i]
         try:
             if string[i] == string[i + 1]:
-                tmpArr[1] = 'X'
+                tmp_arr[1] = 'X'
                 i -= 1
             else:
-                tmpArr[1] = string[i + 1]
+                tmp_arr[1] = string[i + 1]
         except:
-            tmpArr[1] = 'X'
-        playFair.append(tmpArr)
+            tmp_arr[1] = 'X'
+        play_fair.append(tmp_arr)
         i += 2
 
-    for i in range(len(playFair)):
-        print(playFair[i][0] + "" + playFair[i][1] + "")
-    for i in range(len(playFair)):
-        tmpArr = [[], []]
+    for i in range(len(play_fair)):
+        tmp_arr = [[], []]
         for j in range(len(board)):
             for k in range(len(board[j])):
-                if playFair[i][0] == board[j][k]:
+                if play_fair[i][0] == board[j][k]:
                     x1 = j
                     y1 = k
-                if playFair[i][1] == board[j][k]:
+                if play_fair[i][1] == board[j][k]:
                     x2 = j
                     y2 = k
         if x1 == x2:
-            tmpArr[0] = board[x1][(y1+1) % 5]
-            tmpArr[1] = board[x2][(y2+1) % 5]
+            tmp_arr[0] = board[x1][(y1+1) % 5]
+            tmp_arr[1] = board[x2][(y2+1) % 5]
         elif y1 == y2:
-            tmpArr[0] = board[(x1+1) % 5][y1]
-            tmpArr[1] = board[(x2+1) % 5][y2]
+            tmp_arr[0] = board[(x1+1) % 5][y1]
+            tmp_arr[1] = board[(x2+1) % 5][y2]
         else:
-            tmpArr[0] = board[x2][y1]
-            tmpArr[1] = board[x1][y2]
+            tmp_arr[0] = board[x2][y1]
+            tmp_arr[1] = board[x1][y2]
 
-        encPlayFair.append(tmpArr)
+        enc_play_fair.append(tmp_arr)
 
-    for i in range(len(encPlayFair)):
-        encStr += str(encPlayFair[i][0]) + "" + str(encPlayFair[i][1]) + ""
+    for i in range(len(enc_play_fair)):
+        result += enc_play_fair[i][0] + "" + enc_play_fair[i][1] + ""
 
-    return encStr
+    return result
+
+
+def Decryption(PW, PWkey):
+    board = ["", "", "", "", ""], \
+            ["", "", "", "", ""], \
+            ["", "", "", "", ""], \
+            ["", "", "", "", ""], \
+            ["", "", "", "", ""]
+    key = 중복문자열제거(PWkey).upper()
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXY"
+    al = ""
+    PW = PW.replace(" ", "")
+    PW = PW.upper()
+
+    for a in alphabet:
+        if a not in key:
+            al += a
+    alphabet = al
+
+    count = 0
+    for i in range(5):
+        for j in range(5):
+            if board[i][j] == "":
+                board[i][j] += key[count]
+            if count < len(key):
+                count += 1
+            if count == len(key):
+                break
+        if count == len(key):
+            break
+
+    count = 0
+    for i in range(5):
+        for j in range(5):
+            if not board[i][j]:
+                board[i][j] += alphabet[count]
+                if count < len(alphabet):
+                    count += 1
+                if count == len(alphabet):
+                    break
+        if count == len(alphabet):
+            break
+
+    play_fair = []
+    dec_play_fair = []
+    x1 = 0
+    x2 = 0
+    y1 = 0
+    y2 = 0
+    result = ""
+
+    i = 0
+    while i < len(PW):
+        tmp_arr = [[], []]
+        tmp_arr[0] = PW[i]
+        tmp_arr[1] = PW[i+1]
+        play_fair.append(tmp_arr)
+        i += 2
+
+    for i in range(len(play_fair)):
+        tmp_arr = [[], []]
+        for j in range(len(board)):
+            for k in range(len(board[j])):
+                if play_fair[i][0] == board[j][k]:
+                    x1 = j
+                    y1 = k
+                if play_fair[i][1] == board[j][k]:
+                    x2 = j
+                    y2 = k
+        if x1 == x2:
+            tmp_arr[0] = board[x1][(y1+4) % 5]
+            tmp_arr[1] = board[x2][(y2+4) % 5]
+        elif y1 == y2:
+            tmp_arr[0] = board[(x1+4) % 5][y1]
+            tmp_arr[1] = board[(x2+4) % 5][y2]
+        else:
+            tmp_arr[0] = board[x2][y1]
+            tmp_arr[1] = board[x1][y2]
+
+        dec_play_fair.append(tmp_arr)
+
+    for i in range(len(dec_play_fair)):
+        if (i != (len(dec_play_fair)-1)) and (dec_play_fair[i][1] == 'X') and (dec_play_fair[i][0] == dec_play_fair[i+1][0]):
+            result += dec_play_fair[i][0]
+        else:
+            result += dec_play_fair[i][0] + "" + dec_play_fair[i][1]
+
+    return result
 
